e0bfddb7510d9c391a9b2e0396ac23e60b2f076a
jemsley canela <jemscanela@gmail.com>
2018-05-23 15:48:22 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,22 @@
+chocolateBars= ["snickers","hundred grand","kitkat","skittles"];
+
+function addElementToBeginningOfArray(a,b){
+  return [b,...a]
+}
+function destructivelyAddElementToBeginningOfArray(a,b){
+   a.unshift(b);
+   return a;
+}
+
+function addElementToEndOfArray(a,b) {
+  return [...a,b]
+}
+
+function destructivelyAddElementToEndOfArray(a,b){
+  a.push(b)
+  return a
+}
+
+function accessElementInArray(){
+  return a[b]
+}
